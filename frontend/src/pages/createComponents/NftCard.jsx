d6a551fe0ca7f6619ecde3bb32e55adf6bf368da
Zayyan <zayyan.pluton@gmail.com>
2023-07-07 14:40:27 +0500

    changes done 2

--- frontend/src/pages/createComponents/NftCard.jsx
+++ frontend/src/pages/createComponents/NftCard.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const NftCard = ({ isClicked, isCompleted, index, img, handleRemoveImage }) => {
-    return (
-        <div >
-            <div className={`${isClicked === "true" ? 'nft-is-clicked' : ""}`} key={index}>
-                <img src={img} alt="" />
-                <button className="remove-nft-image" onClick={() => handleRemoveImage(index)}>x</button>
-            </div>
-            {
-                isCompleted === "true" &&
-                <div className='Completed-layer'>
-                    COMPLETED
-                </div>
-            }
-        </div>
-    )
-}
+  console.log(img, "this is image");
+  return (
+    <div>
+      <div
+        className={`${isClicked === "true" ? "nft-is-clicked" : ""}`}
+        key={index}
+      >
+        <img src={img} alt="" />
+        <button
+          className="remove-nft-image"
+          onClick={() => handleRemoveImage(index)}
+        >
+          x
+        </button>
+      </div>
+      {isCompleted === "true" && (
+        <div className="Completed-layer">COMPLETED</div>
+      )}
+    </div>
+  );
+};
 
-export default NftCard
+export default NftCard;
