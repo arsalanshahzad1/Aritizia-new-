ecca3b70f437a63b341c8b249182be9c7e161791
Ali_monis <127748052+Ali-Monis-Pluton@users.noreply.github.com>
2024-01-19 16:16:32 +0500

    three

--- frontend/src/assets/constent/index.js
+++
@@ -1,3 +0,0 @@
-
-export const Local_URL = "http://127.0.0.1:5173/";
-export const Live_URl = "";
--- frontend/src/contextApi/walletContext.js
+++
@@ -1,5 +0,0 @@
-// import { createContext } from "react";
-
-// const walletContext = createContext();
-
-// export default walletContext;
